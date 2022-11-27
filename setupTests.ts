64bed394e1a771545b41eedd69395be53bc97ee6
SafeAndS0und <nikodem.lorenz@gmail.com>
2022-11-27 17:11:28 +0000

    Deploying to gh-pages from @ SafeAndS0und/github-autocomplete-component@805b27a90b9d4573a32241112b98a59d4a589987 🚀

--- setupTests.ts
+++
@@ -1,5 +0,0 @@
-import server from "./src/mocks/msw";
-
-beforeAll(() => server.listen({ onUnhandledRequest: "error" }));
-afterEach(() => server.resetHandlers());
-afterAll(() => server.close());
--- src/pages/_app.tsx
+++
@@ -1,6 +0,0 @@
-import type { AppProps } from "next/app";
-import "../common/globals.css";
-
-export default ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
-};
